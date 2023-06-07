935c8450d9c0a9bd0a44eea78efcae8052266921
Kashif-Rezwi <Kashifrezwi850@gmail.com>
2023-06-07 06:49:35 +0530

    backend websocket server created

---
+++ backend/node_modules/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const WebSocket = require("ws");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Server is on air.");
+});
+
+const server = app.listen(8080, () => {
+  console.log("Server started on port 8080");
+});
+
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (client) => {
+  console.log("New WebSocket Connection.");
+
+  client.on("message", (msg) => {
+    // parsing the buffer data
+    const messageData = JSON.parse(msg);
+    console.log("Received", messageData);
+
+    // sends message to all the clients except himself
+    [...wss.clients]
+      .filter((c) => c !== client)
+      .forEach((el) => el.send(JSON.stringify(messageData))); // stringify the parse data
+  });
+
+  client.on("close", () => {
+    console.log("WebSocket Connection Closed.");
+  });
+});
